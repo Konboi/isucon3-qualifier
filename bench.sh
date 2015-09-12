88791e791537a0c88f43b352711c4ecc88890819
Konboi <ryosuke.yabuki@gmail.com>
2015-09-12 17:23:49 +0900

    やはり無理がある

--- bench.sh
+++ bench.sh
@@ -1,5 +1,5 @@
 mysql -uisucon -pisucon isucon < /home/isucon/webapp/config/schema.sql
 cd /opt/isucon/data && pigz -dc init.sql.gz | mysql -uisucon -pisucon isucon
 mysql -uisucon -pisucon isucon < /home/isucon/webapp/config/index.sql
-cd /home/isucon/webapp/ruby && /home/isucon/env.sh bundle exec ruby prepare.rb
+# cd /home/isucon/webapp/ruby && /home/isucon/env.sh bundle exec ruby prepare.rb
 /home/isucon/bench test
