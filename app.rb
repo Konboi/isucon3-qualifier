a0928358f768a1106488c557bb20dc7853a4162d
Konboi <ryosuke.yabuki@gmail.com>
2015-09-12 18:30:52 +0900

    Revert "fix param name"
    This reverts commit 7817442c731757b82c5f0f576a78b37a8fb16aaa.

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ require "rack/session/redis"
 class Isucon3App < Sinatra::Base
   use Rack::Session::Redis, {
     :expire_after => 60,
-    :namespace    => 'isucon_session',
+    :key          => 'isucon_session',
   }
 
   helpers do
