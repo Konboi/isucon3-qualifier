22f8d4fa9f040458b6c31f5fea1cffa7a39d4412
Konboi <ryosuke.yabuki@gmail.com>
2015-09-12 18:02:58 +0900

    created_at もindexに含める

--- config/add.sql
+++ config/add.sql
@@ -1,3 +1,3 @@
-ALTER TABLE memos ADD INDEX `is_private_idx` (`is_private`);
-ALTER TABLE memos ADD INDEX `user_idx` (`user`);
+ALTER TABLE memos ADD INDEX `memos_is_private_created_at_idx` (`is_private`, `created_at`);
+ALTER TABLE memos ADD INDEX `memos_user_created_at_idx` (`user`, `created_at`);
 ALTER TABLE memos ADD username varchar(255) NOT NULL;
