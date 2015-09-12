f04bd161d6fbe5c3d449096c83498ade70bd1481
Konboi <ryosuke.yabuki@gmail.com>
2015-09-12 17:14:43 +0900

    Add pass

--- bench.sh
+++ bench.sh
@@ -1,5 +1,5 @@
-mysql -uisucon isucon < /home/isucon/webapp/config/schema.sql
-cd /opt/isucon/data && pigz -dc init.sql.gz | mysql -uisucon isucon
-mysql -uisucon isucon < /home/isucon/webapp/config/index.sql
+mysql -uisucon -pisucon isucon < /home/isucon/webapp/config/schema.sql
+cd /opt/isucon/data && pigz -dc init.sql.gz | mysql -uisucon -pisucon isucon
+mysql -uisucon -pisucon isucon < /home/isucon/webapp/config/index.sql
 # cd /home/isucon/webapp/ruby && /home/isucon/env.sh bundle exec ruby prepare.rb
 /home/isucon/bench test
